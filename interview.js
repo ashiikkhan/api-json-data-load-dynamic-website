3d3177c8dca394701b0de989d2be254652bf98fd
ashiikkhan <ashiksust06@gmail.com>
2022-08-27 19:16:39 +0600

    interview q

---
+++ interview.js
@@ -0,0 +1,14 @@
+/**
+ * Interview Questions:
+ * 1. what is an api?
+ * 2. what are the HTTP method supported by REST?
+ * 3. can you use GET request instead of PUT to create a resource?
+ * 4. what is the difference between PUT and POST ?
+ * 5. wha is JSON ?
+ * 6. What are CRUD operations?
+ * 7. what is the file extension of JSON?
+ * 8. what are the data types supported by JSON ?
+ * 9.what is the role of JSON.stringigy?
+ * 10. show how to parse a JSON.
+ * 11. GET VS POST.
+ */
